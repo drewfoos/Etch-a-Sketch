57cb4fc02d43540e8ce708ff98765adb575e9b69
drewfoos <drewfoos@yahoo.com>
2024-06-13 11:51:28 -0400

    New board button working

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const container = document.querySelector(".container");
-for (i = 0; i < 256; i++) {
-  const squareDiv = document.createElement("div");
-  squareDiv.className = "grid";
-  container.appendChild(squareDiv);
-  squareDiv.addEventListener("mouseover", () => {
-    squareDiv.classList.add("trail");
-  });
-  container.appendChild(squareDiv);
-}
+
+let makeBoard = (gridSize) => {
+  for (i = 0; i < gridSize ** 2; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.className = "grid";
+    squareDiv.style.width = `calc(100% / ${gridSize})`;
+    squareDiv.style.height = `calc(100% / ${gridSize})`;
+    container.appendChild(squareDiv);
+    squareDiv.addEventListener("mouseover", () => {
+      squareDiv.classList.add("trail");
+    });
+    container.appendChild(squareDiv);
+  }
+};
+
+let resetBoard = () => {
+  let gridSize = prompt("Enter new grid size (e.g. 16 for 16x16)", "16");
+  if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert("Input value must be below 100");
+    return;
+  }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  makeBoard(gridSize);
+};
+
+makeBoard(16);
